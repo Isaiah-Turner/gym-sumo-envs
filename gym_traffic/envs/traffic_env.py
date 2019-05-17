4540719afe4a96a8fec9d2416ba23ffb206e2ea3
Isaiah-Turner <isaiahturner42@gmail.com>
2019-05-17 09:21:26 -0400

    Changed simulation to stop when no cars left

--- gym_traffic/envs/traffic_env.py
+++ gym_traffic/envs/traffic_env.py
@@ -147,7 +147,7 @@ class TrafficEnv(Env):
         traci.simulationStep()
         observation = self._observation()
         reward = self._reward()
-        done = self.sumo_step > self.simulation_end
+        done = not traci.simulation.getMinExpectedNumber() > 0 #self.sumo_step > self.simulation_end
         self.screenshot()
         if done:
             self.stop_sumo()
